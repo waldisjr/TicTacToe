e17aa029f14af22c332a0768b509e03f380ed619
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-03-27 12:45:08 +0200

    static

--- static/js/ttt.js
+++ static/js/ttt.js
@@ -2,6 +2,8 @@ let moveType = "X";
 
 function clearField(){
     let tags = document.getElementsByClassName("field");
+    let flash = document.getElementById("flash")
+    flash.textContent = ''
     for(i in tags){
         tags[i].textContent = "";
     }
@@ -10,7 +12,11 @@ function clearField(){
 
 
 function makeMove(field){
-    if(field.textContent === ''){
+    let flash = document.getElementById('flash')
+    if (flash.textContent != ''){
+        alert('Game finished !\nPlease click restart.')
+    }
+    else if(field.textContent === ''){
         field.textContent = moveType;
         isWin()
         if(moveType === 'X'){
@@ -28,44 +34,38 @@ function makeMove(field){
 
 function isWin(){
     let tags = document.getElementsByClassName("field");
+    let flash = document.getElementById('flash')
+
     if (tags[0].textContent === tags[1].textContent && tags[1].textContent === tags[2].textContent
-        && tags[0].textContent != ''){
-        alert(`${tags[0].textContent} has won`)
-        clearField()
+        && tags[0].textContent != '') {
+        flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[3].textContent === tags[4].textContent && tags[4].textContent === tags[5].textContent
         && tags[3].textContent != ''){
-        alert(`${tags[3].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[6].textContent === tags[7].textContent && tags[7].textContent === tags[8].textContent
         && tags[6].textContent != ''){
-        alert(`${tags[6].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[0].textContent === tags[3].textContent && tags[3].textContent === tags[6].textContent
         && tags[0].textContent != ''){
-        alert(`${tags[0].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[1].textContent === tags[4].textContent && tags[4].textContent === tags[7].textContent
         && tags[1].textContent != ''){
-        alert(`${tags[1].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[1].textContent} has won`
     }
     if (tags[2].textContent === tags[5].textContent && tags[5].textContent === tags[8].textContent
         && tags[2].textContent != ''){
-        alert(`${tags[2].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[2].textContent} has won`
     }
     if (tags[0].textContent === tags[4].textContent && tags[4].textContent === tags[8].textContent
         && tags[0].textContent != ''){
-        alert(`${tags[0].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[2].textContent === tags[4].textContent && tags[4].textContent === tags[6].textContent
         && tags[2].textContent != ''){
-        alert(`${tags[2].textContent} has won`)
-        clearField()
+        flash.textContent = `${tags[0].textContent} has won`
     }
 }
