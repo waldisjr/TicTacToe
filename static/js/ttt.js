ed9407ab4e58bc1833687e2e8c3e82a231bd7307
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-03-27 22:34:11 +0200

    static

--- static/js/ttt.js
+++ static/js/ttt.js
@@ -3,6 +3,7 @@ let moveType = "X";
 function clearField(){
     let tags = document.getElementsByClassName("field");
     let flash = document.getElementById("flash")
+    let i;
     flash.textContent = ''
     for(i in tags){
         tags[i].textContent = "";
@@ -13,12 +14,13 @@ function clearField(){
 
 function makeMove(field){
     let flash = document.getElementById('flash')
-    if (flash.textContent != ''){
+    if (flash.textContent !== ''){
         alert('Game finished !\nPlease click restart.')
     }
     else if(field.textContent === ''){
         field.textContent = moveType;
         isWin()
+        isDraw()
         if(moveType === 'X'){
             moveType = 'O';
         }
@@ -32,40 +34,59 @@ function makeMove(field){
 }
 
 
+function isDraw(){
+    console.log('isDraw')
+    let tags = document.getElementsByClassName("field");
+    let flash = document.getElementById('flash');
+    let i;
+    let flag = true;
+    for (i in tags){
+        console.log(tags[i])
+        if (tags[i].textContent === ''){
+            flag = false
+        }
+    }
+    if (flag === true && flash.textContent === ''){
+        flash.textContent = 'Draw !'
+    }
+}
+
+
+
 function isWin(){
     let tags = document.getElementsByClassName("field");
     let flash = document.getElementById('flash')
 
     if (tags[0].textContent === tags[1].textContent && tags[1].textContent === tags[2].textContent
-        && tags[0].textContent != '') {
+        && tags[0].textContent !== '') {
         flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[3].textContent === tags[4].textContent && tags[4].textContent === tags[5].textContent
-        && tags[3].textContent != ''){
+        && tags[3].textContent !== ''){
         flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[6].textContent === tags[7].textContent && tags[7].textContent === tags[8].textContent
-        && tags[6].textContent != ''){
+        && tags[6].textContent !== ''){
         flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[0].textContent === tags[3].textContent && tags[3].textContent === tags[6].textContent
-        && tags[0].textContent != ''){
+        && tags[0].textContent !== ''){
         flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[1].textContent === tags[4].textContent && tags[4].textContent === tags[7].textContent
-        && tags[1].textContent != ''){
+        && tags[1].textContent !== ''){
         flash.textContent = `${tags[1].textContent} has won`
     }
     if (tags[2].textContent === tags[5].textContent && tags[5].textContent === tags[8].textContent
-        && tags[2].textContent != ''){
+        && tags[2].textContent !== ''){
         flash.textContent = `${tags[2].textContent} has won`
     }
     if (tags[0].textContent === tags[4].textContent && tags[4].textContent === tags[8].textContent
-        && tags[0].textContent != ''){
+        && tags[0].textContent !== ''){
         flash.textContent = `${tags[0].textContent} has won`
     }
     if (tags[2].textContent === tags[4].textContent && tags[4].textContent === tags[6].textContent
-        && tags[2].textContent != ''){
+        && tags[2].textContent !== ''){
         flash.textContent = `${tags[0].textContent} has won`
     }
 }
