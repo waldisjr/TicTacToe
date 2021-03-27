78843cc2c1d843b58b4ca58245296d7f31b20d84
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-03-27 12:32:49 +0200

    static

--- static/js/ttt.js
+++ static/js/ttt.js
@@ -12,6 +12,7 @@ function clearField(){
 function makeMove(field){
     if(field.textContent === ''){
         field.textContent = moveType;
+        isWin()
         if(moveType === 'X'){
             moveType = 'O';
         }
@@ -24,4 +25,47 @@ function makeMove(field){
     }
 }
 
-function isWin(){}
+
+function isWin(){
+    let tags = document.getElementsByClassName("field");
+    if (tags[0].textContent === tags[1].textContent && tags[1].textContent === tags[2].textContent
+        && tags[0].textContent != ''){
+        alert(`${tags[0].textContent} has won`)
+        clearField()
+    }
+    if (tags[3].textContent === tags[4].textContent && tags[4].textContent === tags[5].textContent
+        && tags[3].textContent != ''){
+        alert(`${tags[3].textContent} has won`)
+        clearField()
+    }
+    if (tags[6].textContent === tags[7].textContent && tags[7].textContent === tags[8].textContent
+        && tags[6].textContent != ''){
+        alert(`${tags[6].textContent} has won`)
+        clearField()
+    }
+    if (tags[0].textContent === tags[3].textContent && tags[3].textContent === tags[6].textContent
+        && tags[0].textContent != ''){
+        alert(`${tags[0].textContent} has won`)
+        clearField()
+    }
+    if (tags[1].textContent === tags[4].textContent && tags[4].textContent === tags[7].textContent
+        && tags[1].textContent != ''){
+        alert(`${tags[1].textContent} has won`)
+        clearField()
+    }
+    if (tags[2].textContent === tags[5].textContent && tags[5].textContent === tags[8].textContent
+        && tags[2].textContent != ''){
+        alert(`${tags[2].textContent} has won`)
+        clearField()
+    }
+    if (tags[0].textContent === tags[4].textContent && tags[4].textContent === tags[8].textContent
+        && tags[0].textContent != ''){
+        alert(`${tags[0].textContent} has won`)
+        clearField()
+    }
+    if (tags[2].textContent === tags[4].textContent && tags[4].textContent === tags[6].textContent
+        && tags[2].textContent != ''){
+        alert(`${tags[2].textContent} has won`)
+        clearField()
+    }
+}
